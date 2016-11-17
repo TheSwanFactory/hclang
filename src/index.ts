7994a3407bfd5650a02b2cc79b11ea603597b861
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-11-17 06:27:07 -0800

    Add and use modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { Greeter } from "./greeter";
 
 export { Greeter };
+
+var clear = require("clear");
+var chalk = require("chalk");
+var figlet = require("figlet");
+
+clear();
+console.log(
+  chalk.green(
+    figlet.textSync('MAML', { horizontalLayout: 'full' })
+  )
+);
