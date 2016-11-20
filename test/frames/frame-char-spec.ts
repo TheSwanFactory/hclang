c7846fc40c246bbac3343bc9ed84630637cce7d6
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-11-20 07:38:28 -0800

    Add spaces to specs

--- test/frames/frame-char-spec.ts
+++ test/frames/frame-char-spec.ts
@@ -6,9 +6,11 @@ const expect = chai.expect;
 
 describe("FrameChar", () => {
   const frame_char = new FrameChar("a");
+
   it("takes a string of length one", () => {
     expect(frame_char).to.be.instanceOf(FrameChar);
   });
+
   it("stringifies to a unit netstring", () => {
     expect(frame_char.toString()).to.equal("\\\\a");
   });
--- test/frames/frame-string-spec.ts
+++ test/frames/frame-string-spec.ts
@@ -6,9 +6,11 @@ const expect = chai.expect;
 
 describe("FrameString", () => {
   const frame_string = new FrameString("Hello, MAML!");
+
   it("takes a JavaScript string", () => {
     expect(frame_string).to.be.instanceOf(FrameString);
   });
+
   it("stringifies with smart quotes", () => {
     expect(frame_string.toString()).to.equal("\\\\a");
   });
