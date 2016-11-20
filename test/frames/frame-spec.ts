f4e7950305837fa9302cb21b0257346481178cbf
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-11-20 11:10:38 -0800

    Cleanup FrameArray expectations

--- test/frames/frame-spec.ts
+++ test/frames/frame-spec.ts
@@ -5,7 +5,7 @@ import * as chai from "chai";
 const expect = chai.expect;
 
 describe("Frame", () => {
-  it("should exist", () => {
+  it("is constructed from nothing", () => {
     const frame = new Frame();
     expect(frame).to.be.instanceOf(Frame);
   });
@@ -15,16 +15,13 @@ describe("FrameArray", () => {
   const frame = new Frame();
   const frame_array = new FrameArray([frame]);
 
-  it("should exist", () => {
+  it("is constructed from an array of frames", () => {
     expect(frame_array).to.be.instanceOf(FrameArray);
   });
 
   it("uses 'at' to access elements by index", () => {
-    let element = frame_array.at(0);
-    expect(element).to.be.instanceOf(Frame);
-  });
-
-  it("is constructed from an array of frames", () => {
-    expect(frame_array).to.be.instanceOf(FrameArray);
+    const first_element = frame_array.at(0);
+    expect(first_element).to.be.ok;
+    expect(first_element).to.be.instanceOf(Frame);
   });
 });
