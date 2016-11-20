d4866ed70804f1fe12d9a0e8ef76e6c30ad71a0a
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-11-20 07:40:44 -0800

    Add FrameArray to Frame spec

--- test/frames/frame-spec.ts
+++ test/frames/frame-spec.ts
@@ -1,5 +1,5 @@
 
-import { Frame } from "../../src/frames/frame";
+import { Frame, FrameArray } from "../../src/frames/frame";
 import * as chai from "chai";
 
 const expect = chai.expect;
@@ -8,5 +8,12 @@ describe("Frame", () => {
   it("should exist", () => {
     const frame = new Frame();
     expect(frame).to.be.instanceOf(Frame);
+  });
+});
+
+describe("FrameArray", () => {
+  it("should exist", () => {
+    const frame = new FrameArray();
+    expect(frame).to.be.instanceOf(FrameArray);
   });
 });
