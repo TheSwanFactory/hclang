bb912f352bda71eda1508995aa6f417e9b1095c5
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-11-21 11:55:05 -0800

    stub FrameExpr

---
+++ src/frames/frame-expr.ts
@@ -0,0 +1,22 @@
+import { Frame, FrameArray } from "./frame";
+
+export class FrameExpr extends FrameArray {
+  public static readonly BEGIN = "(";
+  public static readonly END = ")";
+
+  constructor(data: Array<Frame>) {
+    super(data);
+  }
+
+  public call(argument: Frame) {
+    return this;
+  }
+
+  public toStringData() {
+    return this.data.map((obj: Frame) => { return obj.toString(); }).join(" ");
+  };
+
+  public toString() {
+    return FrameExpr.BEGIN + this.toStringData() + FrameExpr.END;
+  }
+};
