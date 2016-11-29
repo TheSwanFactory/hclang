fc7512e8db633486c94b102077245ff73fce58d0
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-11-29 06:09:47 -0800

    Create FrameName
    Returns a symbol when evaluated

---
+++ src/frames/frame-name.ts
@@ -0,0 +1,31 @@
+import { Frame, Void } from "./frame";
+import { FrameSymbol } from "./frame-symbol";
+
+export class FrameName extends Frame {
+  protected data: FrameSymbol;
+
+  constructor(symbol: string, meta = Void) {
+    super(meta);
+    this.data = FrameSymbol.for(symbol);
+  }
+
+  public in(context = Frame.nil) {
+    return this.data;
+  }
+
+  public called_by(context: Frame) {
+    return this.in(context);
+  }
+
+  public toStringData() {
+    return "." + this.data.toString();
+  }
+
+  public toString() {
+    const meta = this.meta_string();
+    if (meta !== "") {
+      return `(${meta} ${this.toStringData()})`;
+    }
+    return this.toStringData();
+  }
+};
