1a07e62c8088ff937433b975dbed42a75967b9a7
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-02 05:52:08 -0800

    Re-export Frames from frames.ts

---
+++ lib/frames.d.ts
@@ -0,0 +1,6 @@
+export { FrameChar } from "./frames/frame-char";
+export { FrameExpr } from "./frames/frame-expr";
+export { FrameName } from "./frames/frame-name";
+export { FrameString } from "./frames/frame-string";
+export { FrameSymbol } from "./frames/frame-symbol";
+export { IKeyValuePair, Context, Frame, FrameArray, Void } from "./frames/frame";
