d6c144289049defda748915e1977bc0d2350cbb7
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-05 06:19:39 -0800

    Refactor spec

--- test/maml/maml-html-spec.ts
+++ test/maml/maml-html-spec.ts
@@ -1,16 +1,14 @@
 import { expect } from "chai";
 import { Frame, FrameExpr, FrameString, FrameSymbol } from "../../src/frames";
 
-const HTML_PREFIX = "<!DOCTYPE html>"
-
-
-let html_wrap = (tag: string, content: Frame) => {
+const html_wrap = (tag: string, content: Frame) => {
   return new FrameExpr([
     new FrameString(`<${tag}>`), content, new FrameString(`</${tag}>`)
   ]);
 };
 
-let html_call = (content: FrameString) => {
+const HTML_PREFIX = "<!DOCTYPE html>"
+const html_call = (content: FrameString) => {
   const html = new FrameExpr([
     new FrameString(HTML_PREFIX),
     html_wrap("head", html_wrap("title", content.get("title"))),
@@ -18,7 +16,6 @@ let html_call = (content: FrameString) => {
   ]);
   return html.in(Frame.nil);
 };
-
 
 describe("FrameHTML", () => {
   const js_string = "Hello, HTML!";
@@ -31,15 +28,17 @@ describe("FrameHTML", () => {
   const result = html_call(html_content);
   const result_string = result.toString();
 
-  it("embeds properties into head",  () => {
+  it("returns a FrameString",  () => {
     expect(result).to.be.an.instanceof(FrameString);
+  });
+
+  it("embeds properties into head",  () => {
     expect(result_string).to.match(/<head>([\s\S]*)<\/head>/);
     expect(result_string).to.match(/<title>([\s\S]*)<\/title>/);
     expect(result_string).to.include(js_title);
   });
 
   it("HTML-ifies string expressions when called", () => {
-    expect(result).to.be.an.instanceof(FrameString);
     expect(result_string).to.include('<!DOCTYPE html>');
     expect(result_string).to.match(/<body>([\s\S]*)<\/body>/);
     expect(result_string).to.include(js_string);
