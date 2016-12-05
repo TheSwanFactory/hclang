c8623d9d5f1020799e72d14e72cad11aa71ac686
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-05 06:18:12 -0800

    move content inside describe

--- test/maml/maml-html-spec.ts
+++ test/maml/maml-html-spec.ts
@@ -2,25 +2,7 @@ import { expect } from "chai";
 import { Frame, FrameExpr, FrameString, FrameSymbol } from "../../src/frames";
 
 const HTML_PREFIX = "<!DOCTYPE html>"
-const BEGIN_HTML = `
-${HTML_PREFIX}
-<html>
-<head>
-</head>
-<body>
-`;
 
-const END_HTML = `
-</body>
-</html>
-`;
-
-const js_string = "Hello, HTML!";
-const js_title = "First HTML6 File";
-const html_content = new FrameString(js_string, {
-    title:new FrameString(js_title)
-  }
-);
 
 let html_wrap = (tag: string, content: Frame) => {
   return new FrameExpr([
@@ -31,16 +13,23 @@ let html_wrap = (tag: string, content: Frame) => {
 let html_call = (content: FrameString) => {
   const html = new FrameExpr([
     new FrameString(HTML_PREFIX),
-    html_wrap("head", html_wrap("title", html_content.get("title"))),
+    html_wrap("head", html_wrap("title", content.get("title"))),
     html_wrap("body", content),
   ]);
   return html.in(Frame.nil);
 };
 
-const result = html_call(html_content);
-const result_string = result.toString();
 
 describe("FrameHTML", () => {
+  const js_string = "Hello, HTML!";
+  const js_title = "First HTML6 File";
+  const html_content = new FrameString(js_string, {
+      title:new FrameString(js_title)
+    }
+  );
+
+  const result = html_call(html_content);
+  const result_string = result.toString();
 
   it("embeds properties into head",  () => {
     expect(result).to.be.an.instanceof(FrameString);
