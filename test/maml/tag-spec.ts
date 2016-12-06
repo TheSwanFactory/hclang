8b06614f75cfb90b81c56931e9d8d1fef0ac7cc0
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-06 06:24:37 -0800

    evaluates to a FrameString

--- test/maml/tag-spec.ts
+++ test/maml/tag-spec.ts
@@ -1,11 +1,16 @@
 import { expect } from "chai";
-import { FrameExpr } from "../../src/frames";
+import { FrameExpr, FrameString } from "../../src/frames";
 import { tag } from "../../src/maml";
 
 describe("MAML Tag", () => {
-  const result = tag("a");
+  const expr = tag("a");
+  const result = expr.in();
 
   it("returns a FrameExpr", () => {
-    expect(result).to.be.instanceOf(FrameExpr);
+    expect(expr).to.be.instanceOf(FrameExpr);
+  });
+
+  it("evaluates to a FrameString", () => {
+    expect(result).to.be.instanceOf(FrameString);
   });
 });
