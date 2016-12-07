e986175709fd6f51a0f4036f5aad92f977960e00
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-07 05:52:22 -0800

    Stub frame-lazy-specs

--- test/frames/frame-lazy-spec.ts
+++ test/frames/frame-lazy-spec.ts
@@ -2,4 +2,19 @@ import { expect } from "chai";
 import { FrameLazy } from "../../src/frames";
 
 describe("FrameLazy", () => {
+  it("takes a Frame", () => {
+    expect(true).to.be.true;
+  });
+
+  it("returns that Frame when evaluated", () => {
+    expect(true).to.be.true;
+  });
+
+  it("sets that Frame's kUP to the calling context", () => {
+    expect(true).to.be.true;
+  });
+
+  it("stringifies to {frame}", () => {
+    expect(true).to.be.true;
+  });
 });
