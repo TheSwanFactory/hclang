14c85622300e3677958df8369231293bb2b1a1bb
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-08 07:25:39 -0800

    stub tag_lazy

--- src/maml/tag.ts
+++ src/maml/tag.ts
@@ -1,9 +1,25 @@
-import { Frame, FrameExpr, FrameString } from "../frames";
+import { Frame, FrameExpr, FrameLazy, FrameString, FrameSymbol } from "../frames";
 
 export const tag = (name: string, body: Frame) => {
   return new FrameExpr([
     new FrameString(`<${name}>`),
     body,
     new FrameString(`</${name}>`),
+  ]);
+};
+
+const wrap_args = (prefix: string, suffix: string) => {
+  return new FrameExpr([
+    new FrameString(prefix),
+    FrameSymbol.here(),
+    new FrameString(suffix),
+  ]);
+}
+
+const tag_lazy = (contents: FrameString) => {
+  return new FrameExpr([
+    wrap_args("<",">"),
+    new FrameLazy(FrameSymbol.here()),
+    wrap_args("</",">"),
   ]);
 };
