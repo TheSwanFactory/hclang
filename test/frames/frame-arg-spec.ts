808dde3a81902bfac873382de53373d319b298a2
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-10 05:52:14 -0800

    Refactor spec into 'here' and 'level'

--- test/frames/frame-arg-spec.ts
+++ test/frames/frame-arg-spec.ts
@@ -1,27 +1,33 @@
 import { expect } from "chai";
 import { FrameArg, FrameString } from "../../src/frames";
 
-
 describe("FrameArg", () => {
   const frame_arg = FrameArg.here();
 
-  it("is created from 'here'", () => {
-    expect(frame_arg).to.be.instanceOf(FrameArg);
+  describe("here", () => {
+    it("is created from 'here'", () => {
+      expect(frame_arg).to.be.instanceOf(FrameArg);
+    });
+
+    it("stringifies to underscore", () => {
+      expect(frame_arg.toString()).to.equal("_");
+    });
+
+    it ("evaluates to the context", () => {
+      const context = new FrameString("context", {atom: frame_arg});
+      expect(FrameArg.here().in(context)).to.equal(context);
+    });
   });
 
-  it("stringifies to underscore", () => {
-    expect(frame_arg.toString()).to.equal("_");
-  });
-
-  it ("'in' returns the context", () => {
-    const context = new FrameString("context", {atom: frame_arg});
-    expect(FrameArg.here().in(context)).to.equal(context);
-  });
-
-describe("level", () => {
-    it("level 1 returns 'here'", () => {
+  describe("level", () => {
+    it("returns 'here' at level 1", () => {
       const frame_level = FrameArg.level();
       expect(frame_arg).to.equal(frame_level);
+    });
+
+    it("returns the same object at each level", () => {
+      const level_2 = FrameArg.level(2);
+      expect(FrameArg.level(2)).to.equal(level_2);
     });
   });
 });
