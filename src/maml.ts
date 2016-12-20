7d3fca8053d9542ad373513c9cdd8fd0f59366dc
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-20 06:33:42 -0800

    Fail html spec

--- src/maml.ts
+++ src/maml.ts
@@ -1,10 +1,12 @@
 import { FrameArg, FrameExpr, FrameString, FrameSymbol } from "./frames";
 import { tag } from "./maml/tag";
 
-const body = new FrameString("body");
+const body = (level = 1) => {
+  return new FrameExpr([
+    new FrameSymbol("tag"),
+    new FrameString("body"),
+    FrameArg.here(),
+  ], {tag});
+};
 
-export const maml = new FrameExpr([
-  new FrameSymbol("tag"),
-  body,
-  FrameArg.here(),
-], {tag});
+export const maml = body();
