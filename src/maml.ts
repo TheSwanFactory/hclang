5320a9260b8e6421d38d1a9417a740e8dc4c0ab1
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-24 07:54:19 -0800

    stub head

--- src/maml.ts
+++ src/maml.ts
@@ -3,6 +3,13 @@ import { tag } from "./maml/tag";
 
 const HTML_PREFIX = "<!DOCTYPE html>";
 
+const head = [
+  new FrameSymbol("tag"),
+  new FrameString("head"),
+  new FrameExpr([
+  ]),
+];
+
 const body = [
   new FrameSymbol("tag"),
   new FrameString("body"),
@@ -14,6 +21,9 @@ export const maml = new FrameExpr([
   new FrameExpr([
     new FrameSymbol("tag"),
     new FrameString("html"),
-    new FrameExpr(body),
+    new FrameExpr([
+      new FrameExpr(head),
+      new FrameExpr(body),
+    ]),
   ]),
 ], {tag});
