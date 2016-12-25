e6123df14ab17ea1fe8a8c8c5567f90364dced27
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-25 07:31:14 -0800

    Add head spec

--- test/maml/maml-spec.ts
+++ test/maml/maml-spec.ts
@@ -12,18 +12,24 @@ describe("maml", () => {
     expect(maml).to.be.instanceOf(FrameExpr);
   });
 
+  it("when called, returns a FrameString", () => {
+    expect(result).to.be.instanceOf(FrameString);
+  });
+
   it("has a tag property", () => {
     const tag = maml.get("tag");
     expect(tag).to.be.instanceOf(FrameExpr);
   });
 
   it("wraps everything in an HTML tag", () => {
-    expect(result).to.be.instanceOf(FrameString);
     expect(result_string).to.match(/<html>([\s\S]*)<\/html>/);
   });
 
-  it("wraps its argument in a body tag", () => {
-    expect(result).to.be.instanceOf(FrameString);
+  it("wraps arg contents in a body tag", () => {
     expect(result_string).to.include(`<body>${body_text}<\/body>`);
+  });
+
+  it("wraps arg metas in a head tag", () => {
+    expect(result_string).to.include(`<head>([\s\S]*)<\/head>`);
   });
 });
