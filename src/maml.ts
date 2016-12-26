f56ffb79aed9ba2a46c3a47c4b1552949657d220
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-12-26 07:03:26 -0800

    Lint make_tag -> MakeTag

--- src/maml.ts
+++ src/maml.ts
@@ -3,7 +3,7 @@ import { tag } from "./maml/tag";
 
 const HTML_PREFIX = "<!DOCTYPE html>";
 
-const make_tag = (name: string, contents: Frame) => {
+const MakeTag = (name: string, contents: Frame) => {
   return  new FrameExpr([
       new FrameSymbol("tag"),
       new FrameString(name),
@@ -11,11 +11,11 @@ const make_tag = (name: string, contents: Frame) => {
     ]);
 };
 
-const title = make_tag("title", FrameExpr.extract("title"));
-const head = make_tag("head", title);
-const body = make_tag("body", FrameArg.here());
+const title = MakeTag("title", FrameExpr.extract("title"));
+const head = MakeTag("head", title);
+const body = MakeTag("body", FrameArg.here());
 
 export const maml = new FrameExpr([
   new FrameString(HTML_PREFIX),
-  make_tag("html", new FrameExpr([head, body])),
+  MakeTag("html", new FrameExpr([head, body])),
 ], {tag});
