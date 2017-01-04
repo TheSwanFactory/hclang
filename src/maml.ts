de26c9324c4eaf92fff17eedc01ea5ced7aa8dd0
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-04 06:30:46 -0800

    Pass MAML specs
    Move import to avoid lint error

--- src/maml.ts
+++ src/maml.ts
@@ -1,5 +1,6 @@
 import { Frame, FrameArg, FrameExpr, FrameLazy, FrameName, FrameParam, FrameString, FrameSymbol } from "./frames";
 import { Ops } from "./ops";
+
 import { tag } from "./maml/tag";
 
 Frame.globals = Ops;
