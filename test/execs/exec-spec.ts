d8b5ee09c8dbd0ee6f8c70ae5a761571a35d8c8b
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-05 05:23:58 -0800

    fails to evaluate

--- test/execs/exec-spec.ts
+++ test/execs/exec-spec.ts
@@ -5,6 +5,16 @@ describe("exec", () => {
   it("quines FrameStrings", () => {
     const input = "“Watson I need you”";
     const result = exec(input);
+
     expect(result).to.equal(input);
+  });
+
+  it("evaluates FrameStrings", () => {
+    const part1 = "“Hello, ”";
+    const part2 = "“World!”";
+    const input = `${part1} ${part2}`
+    const result = exec(input);
+
+    expect(result).to.equal("“Hello, World!”");
   });
 });
