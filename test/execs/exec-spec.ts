ee4fbfe297f5a91c215ed8b20715c0028afd8532
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-05 05:20:37 -0800

    quines

--- test/execs/exec-spec.ts
+++ test/execs/exec-spec.ts
@@ -1,2 +1,10 @@
 import { expect } from "chai";
 import { exec } from "../../src/execs";
+
+describe("exec", () => {
+  it("quines FrameStrings", () => {
+    const input = "“Watson I need you”";
+    const result = exec(input);
+    expect(result).to.equal(input);
+  });
+});
