d2a9a624e9309a4ada8d101680f77bee37d46476
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-09 05:56:54 -0800

    Stub cli spec

---
+++ test/cli/cli-specs.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { exec } from "../../src/syntax";
+
+describe.only("cli", () => {
+  it("evaluates files", () => {
+    expect(true).to.equal(true);
+  });
+});
