ed956d1a21a49392875971837b1fd8495c1bfa43
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-17 05:46:30 -0800

    Command spec

--- test/syntax/script-spec.ts
+++ test/syntax/script-spec.ts
@@ -3,15 +3,24 @@ import { execFileSync } from "child_process";
 
 describe.only("script", () => {
   const hc_bin = "lib/hc.js";
-  const sample_script = "hc/sample.hc";
-  let script_name: string;
+  let title: string;
 
   beforeEach(function () {
-    script_name = this.currentTest.title;
+    title = this.currentTest.title;
   });
 
-  it(sample_script, () => {
-    const result = execFileSync(hc_bin, [script_name]).toString();
-    expect(result).to.equal("“Hello, Homoiconicity!”");
+  describe.only("commands", () => {
+    it("“Hello, Homoiconicity!”", () => {
+      const result = execFileSync(hc_bin, ["-c", title]).toString();
+      expect(result).to.equal(title);
+    });
+  });
+
+  describe("files", () => {
+    const sample_script = "hc/sample.hc";
+    it(sample_script, () => {
+      const result = execFileSync(hc_bin, [title]).toString();
+      expect(result).to.equal("“Hello, Homoiconicity!”");
+    });
   });
 });
