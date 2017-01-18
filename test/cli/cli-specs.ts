d00f6aee81a17d6e1ec5f85077085daa035720d1
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-18 06:25:12 -0800

    De-stub CLI spec

--- test/cli/cli-specs.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { exec } from "../../src/syntax";
-
-describe.only("cli", () => {
-  it("evaluates files", () => {
-    expect(true).to.equal(true);
-  });
-});
