a2578748a0d36f84c349f2ac7aeaa6b1372b7758
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-18 06:23:27 -0800

    Embrace comments returning nil

--- test/syntax/script-spec.ts
+++ test/syntax/script-spec.ts
@@ -15,31 +15,31 @@ describe("script", () => {
     return result.toString().split("\n");
   };
 
-  describe.only("command", () => {
+  describe.only("expression", () => {
     const hello_string = "“Hello, Quine!”";
     const inline_comment = "#Inline#";
     const endline_comment = "#End-of-line\n";
 
     it(hello_string, () => {
-      const result = script(["-c", title]);
+      const result = script(["-e", title]);
       expect(result[0]).to.equal(title);
     });
 
     it(inline_comment, () => {
-      const result = script(["-c", title]);
-      expect(result[0]).to.equal("");
+      const result = script(["-e", title]);
+      expect(result[0]).to.equal("()");
     });
 
     it(endline_comment, () => {
-      const result = script(["-c", title]);
+      const result = script(["-e", title]);
       expect(result.length).to.equal(2);
-      expect(result[0]).to.equal("");
+      expect(result[0]).to.equal("()");
       expect(result[1]).to.equal("");
     });
 
     it(`${hello_string}${inline_comment}`, () => {
-      const result = script(["-c", title]);
-      expect(result.length).to.equal(1);
+      const result = script(["-e", title]);
+      expect(result.length).to.equal(2);
       expect(result.toString()).to.equal(hello_string);
     });
   });
