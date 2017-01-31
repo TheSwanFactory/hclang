11cd3d55406640506bcb0670e7862babc7675d3d
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-01-31 05:13:52 -0800

    ParseToken is constructed from a Frame

--- test/syntax/parse-spec.ts
+++ test/syntax/parse-spec.ts
@@ -1,12 +1,20 @@
 import { expect } from "chai";
 import * as parse from "../../src/syntax/parse";
+import * as frame from "../../src/frames";
 
-describe("Parse", () => {
+describe.only("Parse", () => {
   describe("ParseToken", () => {
+    const content = new frame.FrameString("content");
+    const token = new parse.ParseToken(content);
     it("is exported", () => {
       expect(parse.ParseToken).to.be.ok;
     });
+
+    it("is constructed from a Frame", () => {
+      expect(token).to.be.ok;
+    });
   });
+
   describe("ParsePipe", () => {
     it("is exported", () => {
       expect(parse.ParsePipe).to.be.ok;
