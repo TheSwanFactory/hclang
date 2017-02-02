14d1c253f3391432d959be2b2740b917ec43331b
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-02 06:28:25 -0800

    Remove conflicts

--- lib/syntax/pipeline.d.ts
+++ lib/syntax/pipeline.d.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import { Context, Frame, FrameString } from "../frames";
 export declare class LexParse extends Frame {
     constructor(out: Frame);
     lex_string(input: string): Frame;
     lex(source: FrameString): Frame;
-=======
-import { Context, Frame } from "../frames";
-export declare class EvalPipe extends Frame {
-    constructor(out: Frame, meta?: Context);
->>>>>>> master
 }
 export declare const framify: (input: string, context?: Context) => Frame;
--- src/syntax/pipeline.ts
+++ src/syntax/pipeline.ts
@@ -1,5 +1,4 @@
 import { Context, Frame, FrameArray, FrameLazy, FrameString, FrameSymbol, Void } from "../frames";
-<<<<<<< HEAD
 import { tokens } from "./tokens";
 
 export class LexParse extends Frame {
@@ -11,25 +10,10 @@ export class LexParse extends Frame {
   public lex_string(input: string) {
     const source = new FrameString(input);
     return this.lex(source);
-=======
-import { LexPipe } from "./lex";
-import { ParsePipe } from "./parse";
-
-export class EvalPipe extends Frame {
-  constructor(out: Frame, meta: Context = Void) {
-    super(meta);
-    this.set(Frame.kOUT, out);
->>>>>>> master
   }
 
-<<<<<<< HEAD
   public lex(source: FrameString) {
     return source.reduce(this);
-=======
-  const status = lexer.lex_string(input);
-  if (status !== lexer) {
-    // console.error(`\n* pipe returned ${status}`);
->>>>>>> master
   }
 }
 
