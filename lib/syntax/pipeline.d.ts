36141d696371cafa42a41e110ad08785fde68df7
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-02 06:16:45 -0800

    Extract tokens

--- lib/syntax/pipeline.d.ts
+++ lib/syntax/pipeline.d.ts
@@ -1,4 +1,9 @@
-import { Context, Frame } from "../frames";
+import { Context, Frame, FrameString } from "../frames";
+export declare class LexPipe extends Frame {
+    constructor(out: Frame);
+    lex_string(input: string): Frame;
+    lex(source: FrameString): Frame;
+}
 export declare class EvalPipe extends Frame {
     constructor(out: Frame, meta?: Context);
 }
--- src/syntax/pipeline.ts
+++ src/syntax/pipeline.ts
@@ -1,5 +1,21 @@
 import { Context, Frame, FrameArray, FrameLazy, FrameString, FrameSymbol, Void } from "../frames";
-import { LexPipe } from "./lex";
+import { tokens } from "./tokens";
+
+export class LexPipe extends Frame {
+  constructor(out: Frame) {
+    tokens[Frame.kOUT] = out;
+    super(tokens);
+  }
+
+  public lex_string(input: string) {
+    const source = new FrameString(input);
+    return this.lex(source);
+  }
+
+  public lex(source: FrameString) {
+    return source.reduce(this);
+  }
+}
 
 export class EvalPipe extends Frame {
   constructor(out: Frame, meta: Context = Void) {
