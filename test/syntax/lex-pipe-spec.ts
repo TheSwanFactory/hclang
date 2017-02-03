8c9d8921bcf3330ebbcf0c7de887befffbe95772
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-03 05:47:33 -0800

    construct pipe

--- test/syntax/lex-pipe-spec.ts
+++ test/syntax/lex-pipe-spec.ts
@@ -1,8 +1,16 @@
 import { expect } from "chai";
+import * as frame from "../../src/frames";
 import { LexPipe } from "../../src/syntax/lex-pipe";
 
 describe("LexPipe", () => {
+  const out = new frame.FrameArray([]);
+  const pipe = new LexPipe(out);
+
   it("is exported", () => {
     expect(LexPipe).to.be.ok;
+  });
+
+  it("is constructed from an out parameter", () => {
+    expect(pipe).to.be.ok;
   });
 });
