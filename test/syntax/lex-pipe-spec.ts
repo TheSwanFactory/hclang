d2cbe0f340532721a5a6d333c1de2e14d91a1113
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-03 08:40:50 -0800

    fail ParseTerminal spec

--- test/syntax/lex-pipe-spec.ts
+++ test/syntax/lex-pipe-spec.ts
@@ -1,10 +1,15 @@
 import { expect } from "chai";
 import * as frame from "../../src/frames";
 import { LexPipe } from "../../src/syntax/lex-pipe";
+import * as parse from "../../src/syntax/parse";
 
 describe.only("LexPipe", () => {
-  const out = new frame.FrameArray([]);
-  const pipe = new LexPipe(out);
+  let out: frame.FrameArray;
+  let pipe: LexPipe;
+  beforeEach(() => {
+    out = new frame.FrameArray([]);
+    pipe = new LexPipe(out);
+  });
 
   it("is exported", () => {
     expect(LexPipe).to.be.ok;
@@ -12,5 +17,11 @@ describe.only("LexPipe", () => {
 
   it("is constructed from an out parameter", () => {
     expect(pipe).to.be.ok;
+  });
+
+  it("emits ParseTerminal on empty string", () => {
+    pipe.lex_string("");
+    const result = out.at(0);
+    expect(result).to.be.an.instanceof(parse.ParseTerminal);
   });
 });
