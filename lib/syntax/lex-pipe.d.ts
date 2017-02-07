f80e21cd3ccb7ffa7ad56bc220890e5a0bb83165
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-07 05:21:55 -0800

    class LexPipe extends Lex

--- lib/syntax/lex-pipe.d.ts
+++ lib/syntax/lex-pipe.d.ts
@@ -1,6 +1,8 @@
 import { Frame, FrameString } from "../frames";
-export declare class LexPipe extends Frame {
+import { Lex } from "./lex";
+export declare class LexPipe extends Lex {
     constructor(out: Frame);
     lex_string(input: string): Frame;
     lex(source: FrameString): Frame;
+    finish(): Frame;
 }
--- src/syntax/lex-pipe.ts
+++ src/syntax/lex-pipe.ts
@@ -1,12 +1,13 @@
 import * as _ from "lodash";
 import { Context, Frame, FrameArray, FrameLazy, FrameString, FrameSymbol, Void } from "../frames";
+import { Lex } from "./lex";
 import { terminals } from "./terminals";
 import { tokens } from "./tokens";
 
 const meta = _.clone(tokens);
 _.merge(meta, terminals);
 
-export class LexPipe extends Frame {
+export class LexPipe extends Lex {
   constructor(out: Frame) {
     meta[Frame.kOUT] = out;
     super(meta);
@@ -19,5 +20,9 @@ export class LexPipe extends Frame {
 
   public lex(source: FrameString) {
     return source.reduce(this);
+  }
+
+  public finish() {
+    return Frame.nil;
   }
 }
