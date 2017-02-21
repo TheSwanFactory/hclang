6989d8b75482200aade340ef3d49aad029bcfe34
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-21 06:31:34 -0800

    Fail eval spec

--- test/syntax/eval-pipe-spec.ts
+++ test/syntax/eval-pipe-spec.ts
@@ -7,16 +7,26 @@ import { EvalPipe } from "../../src/syntax/piper";
 
 describe("EvalPipe", () => {
   let out: frame.FrameArray;
+  let pipe: EvalPipe;
 
   beforeEach(() => {
     out = new frame.FrameArray([]);
+    pipe = new EvalPipe(out);
   });
 
   it("is exported", () => {
     expect(EvalPipe).to.be.ok;
   });
 
-  it("Evaluates arguments", () => {
-    //expect(result.toString()).to.equal("()");
+  it("is constructed from an out parameter", () => {
+    expect(pipe).to.be.ok;
+  });
+
+  it("evaluates arguments", () => {
+    const strA = new frame.FrameString("A");
+    const strB = new frame.FrameString("B");
+    const expr = new frame.FrameExpr([strA, strB]);
+    const result = pipe.call(expr);
+    expect(result.toString()).to.equal("“A B”");
   });
 });
