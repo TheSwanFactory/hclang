8334a0fd294afcfa17ca7a51203f54f665ac14a6
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-22 05:51:59 -0800

    returns empty array for empty string

--- test/syntax/piper-spec.ts
+++ test/syntax/piper-spec.ts
@@ -6,20 +6,12 @@ import * as parse from "../../src/syntax/parse";
 import { piper } from "../../src/syntax/piper";
 
 describe("Piper", () => {
-  let out: frame.FrameArray;
-
-  beforeEach(() => {
-    out = new frame.FrameArray([]);
-  });
-
   it("is exported", () => {
     expect(piper).to.be.ok;
   });
 
-  it("returns nil from empty string", () => {
-    expect(out.size()).to.equal(0);
-    const status = piper.call("");
-    expect(out.size()).to.equal(1);
-    //expect(result.toString()).to.equal("()");
+  it("returns empty array for empty string", () => {
+    const result = piper.call("");
+    expect(result.toString()).to.equal("[]");
   });
 });
