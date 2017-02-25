eee1751a11169910db17eca1cb5e7fbe5cd663ba
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-25 03:15:07 -0800

    syntax returns multiple lines
    Join the array.

--- src/syntax.ts
+++ src/syntax.ts
@@ -1,7 +1,9 @@
-// import { Frame } from "./frames";
+import { FrameArray } from "./frames";
 import { evaluate } from "./syntax/evaluate";
 import { framify } from "./syntax/pipeline";
 
 export const exec = (input: string) => {
-  return evaluate(input).toString();
+  const result = evaluate(input) as FrameArray;
+  const array = result.toStringArray();
+  return array.join("\n");
 };
