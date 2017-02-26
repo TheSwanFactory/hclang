dc96c1f3fd934cd8cad28710a1ac84167e5f62c0
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-02-26 07:21:06 -0800

    script spec comments properly

--- test/syntax/script-spec.ts
+++ test/syntax/script-spec.ts
@@ -27,13 +27,13 @@ describe("script", () => {
 
     it(inline_comment, () => {
       const result = script(["-e", title]);
-      expect(result[0]).to.equal("()");
+      expect(result[0]).to.equal("");
     });
 
     it(endline_comment, () => {
       const result = script(["-e", title]);
       expect(result.length).to.equal(2);
-      expect(result[0]).to.equal("()");
+      expect(result[0]).to.equal("");
       expect(result[1]).to.equal("");
     });
   });
