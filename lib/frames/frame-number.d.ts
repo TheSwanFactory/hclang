4fea26c5a7d17bc4aa9d1e33c9a7cada3ff06200
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-03-18 06:33:00 -0700

    FrameNumber from source string

--- lib/frames/frame-number.d.ts
+++ lib/frames/frame-number.d.ts
@@ -1,10 +1,10 @@
 import { Context, FrameAtom } from "./frame";
 export declare class FrameNumber extends FrameAtom {
-    protected data: string;
     static readonly NUMBER_BEGIN: string;
     static readonly NUMBER_END: string;
-    constructor(data: string, meta?: Context);
+    protected data: number;
+    constructor(source: string, meta?: Context);
     string_prefix(): string;
     string_suffix(): string;
-    protected toData(): string;
+    protected toData(): number;
 }
--- src/frames/frame-number.ts
+++ src/frames/frame-number.ts
@@ -1,11 +1,14 @@
+import * as _ from "lodash";
 import { Context, Frame, FrameAtom, Void } from "./frame";
 
 export class FrameNumber extends FrameAtom {
   public static readonly NUMBER_BEGIN = "0-9";
   public static readonly NUMBER_END = "^0-9";
+  protected data: number;
 
-  constructor(protected data: string, meta: Context = Void) {
+  constructor(source: string, meta: Context = Void) {
     super(meta);
+    this.data = _.toNumber(source);
   }
 
   public string_prefix() { return FrameNumber.NUMBER_BEGIN; };
