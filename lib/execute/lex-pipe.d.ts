02889d57f9d433429feb57e39abd8c1a9c8640ca
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-03-31 05:47:02 -0700

    Lex push/pop

--- lib/execute/lex-pipe.d.ts
+++ lib/execute/lex-pipe.d.ts
@@ -1,8 +1,12 @@
 import { Frame, FrameString } from "../frames";
+import { ParsePipe } from "./parse-pipe";
 export declare class LexPipe extends Frame {
     constructor(out: Frame);
     lex_string(input: string): Frame;
     lex(source: FrameString): Frame;
+    parser(): ParsePipe;
+    push(): void;
+    pop(): void;
     finish(): Frame;
     next(): this;
 }
