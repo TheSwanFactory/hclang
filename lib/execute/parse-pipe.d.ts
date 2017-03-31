c169a4dc402c134e43f6414e3b56b7d627703a82
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-03-31 05:40:42 -0700

    Parse push/pop

--- lib/execute/parse-pipe.d.ts
+++ lib/execute/parse-pipe.d.ts
@@ -1,5 +1,7 @@
 import { Frame, FrameArray } from "../frames";
 export declare class ParsePipe extends FrameArray {
     constructor(out: Frame);
+    push(): Frame;
+    pop(): Frame;
     finish(): Frame;
 }
