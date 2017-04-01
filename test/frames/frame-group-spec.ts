1260f4b4167c251333fb2b29dd0bef1bbda40f51
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-04-01 05:44:54 -0700

    Fail single-value spec

--- test/frames/frame-group-spec.ts
+++ test/frames/frame-group-spec.ts
@@ -5,10 +5,22 @@ import * as frame from "../../src/frames";
 describe("FrameGroup", () => {
   const a_frame = new frame.FrameString("a");
   const b_frame = new frame.FrameString("b");
-  const frame_group = new frame.FrameGroup([a_frame, b_frame]);
+  const c_frame = new frame.FrameString("c");
+  const frame_group = new frame.FrameGroup([a_frame], {c: c_frame});
+  const multi_group = new frame.FrameGroup([a_frame, b_frame], {c: c_frame});
 
   it("is constructed from an array of frames", () => {
     expect(frame_group).to.be.instanceOf(frame.FrameGroup);
+  });
+
+  it("with multiple arguments, evaluates to FrameArray", () => {
+    const result = multi_group.in();
+    expect(result).to.be.instanceOf(frame.FrameArray);
+  });
+
+  it("with single argument, evaluates that", () => {
+    const result = frame_group.in();
+    expect(result).to.be.instanceOf(frame.FrameString);
   });
 
 });
