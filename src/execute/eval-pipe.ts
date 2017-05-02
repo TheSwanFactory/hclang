c26b5e619f2c70d1f0f4c3fabf920929e939f6fa
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-05-01 21:50:26 -0700

    EvalPipe symbol

--- src/execute/eval-pipe.ts
+++ src/execute/eval-pipe.ts
@@ -7,8 +7,9 @@ export class EvalPipe extends Frame {
   }
 
   public apply(expr: Frame, context: Frame) {
-    const result = expr.in([context]);
+//    debugger;
     const out = this.get(Frame.kOUT);
+    const result = expr.in([context, out]);
     out.call(result);
     return result;
   }
