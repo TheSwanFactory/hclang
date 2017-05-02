6d47347430d4476e6d9f5b70408862daad863b0f
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-05-01 21:55:28 -0700

    Pass EvalPipe spec

--- src/execute/eval-pipe.ts
+++ src/execute/eval-pipe.ts
@@ -4,10 +4,10 @@ export class EvalPipe extends Frame {
   constructor(out: Frame, meta: Context = NilContext) {
     super(meta);
     this.set(Frame.kOUT, out);
+    this.up = out;
   }
 
   public apply(expr: Frame, context: Frame) {
-//    debugger;
     const out = this.get(Frame.kOUT);
     const result = expr.in([context, out]);
     out.call(result);
