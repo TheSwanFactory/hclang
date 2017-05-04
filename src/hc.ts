7032c348b58257f7b0b57aa62d74f7eb0024fa5f
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-05-04 01:11:41 -0700

    use second argument for file

--- src/hc.ts
+++ src/hc.ts
@@ -4,7 +4,13 @@ import { execute } from "./execute";
 
 let input = "“Hello, MAML!”";
 const argv = process.argv;
-if (argv.length > 3) { input = argv[3]; };
+if (argv.length > 3) {
+  input = argv[3];
+} else if (argv.length === 3) {
+  const file = argv[2];
+  console.log(file);
+  input = file;
+};
 
 const output = execute(input);
 console.log(output);
