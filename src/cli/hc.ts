1f9e4e4b2027d8ca965c0a582297eddc0b6fb91b
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-15 05:42:46 -0700

    build

--- src/cli/hc.ts
+++ src/cli/hc.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
 import * as fs from "fs";
+import * as getopts from "getopts";
+import * as prompt from "prompt-sync";
 import { execute } from "../execute";
 
 let input = "“Hello, MAML!”";
 const argv = process.argv;
+console.log('argv' + argv);
 if (argv.length > 3) {
   input = argv[3];
 } else if (argv.length === 3) {
   const file = argv[2];
   input = fs.readFileSync(file, "utf8");
-  // console.log(input);
+  console.log(input);
 };
 
 const output = execute(input);
