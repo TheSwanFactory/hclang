14873f0fd3eafd362750b16c6bff6f4b9d5e1681
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-15 20:53:23 -0700

    const options = getopts

--- src/cli/hc.ts
+++ src/cli/hc.ts
@@ -6,14 +6,22 @@ import * as prompt from "prompt-sync";
 import { execute } from "../execute";
 
 let input = "“Hello, MAML!”";
-const argv = process.argv;
-// console.log('argv' + argv);
-if (argv.length > 3) {
-  input = argv[3];
-} else if (argv.length === 3) {
-  const file = argv[2];
-  input = fs.readFileSync(file, "utf8");
-  console.log(input);
+
+const options = getopts(process.argv.slice(2), {
+  alias: {
+    eval: "e",
+    help: "h",
+    interactive: "i",
+  },
+});
+
+const files = options._;
+if (files.length > 1) {
+  input = fs.readFileSync(files[0], "utf8");
+} else if (options.eval) {
+  input = options.eval;
+} else {
+  options.interactive = true;
 };
 
 const output = execute(input);
