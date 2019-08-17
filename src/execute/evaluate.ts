2798f1547fc7266f1a4c56c3867da0f6c93b51c9
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-17 04:55:18 -0700

    trim exports

--- src/execute/evaluate.ts
+++ src/execute/evaluate.ts
@@ -1,9 +1,5 @@
-import { Context, Frame, FrameArray, FrameString, NilContext } from "../frames";
-import { EvalPipe } from "./eval-pipe";
-import { GroupPipe } from "./group-pipe";
+import { Context, Frame, NilContext } from "../frames";
 import { HC } from "./hc";
-import { LexPipe } from "./lex-pipe";
-import { ParsePipe } from "./parse-pipe";
 
 export const evaluate = (input: string, context = NilContext): Frame => {
   const hc = new HC(context);
