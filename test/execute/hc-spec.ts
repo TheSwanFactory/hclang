827ba853038b93f85d7998e72f2c80bd9257ef34
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-17 05:08:29 -0700

    use hc.evaluate

--- test/execute/hc-spec.ts
+++ test/execute/hc-spec.ts
@@ -1,74 +1,73 @@
 
 import { expect } from "chai";
 import {} from "mocha";
-import { evaluate } from "../../src/execute/evaluate";
+import { HC } from "../../src/execute/hc";
 import * as frame from "../../src/frames";
 
-describe("evaluate", () => {
-  const success = new frame.FrameString("success!");
-  let out: frame.FrameArray;
+describe.only("HC", () => {
+  let hc: HC;
 
   beforeEach(() => {
-    out = new frame.FrameArray([]);
-    out.set(frame.Frame.kEND, success);
+    hc = new HC();
   });
 
   it("is exported", () => {
-    expect(evaluate).to.be.ok;
+    expect(HC).to.be.ok;
+    expect(hc).to.be.ok;
   });
 
   it("returns empty array for empty string", () => {
-    const result = evaluate.call("");
+    const result = hc.evaluate("");
     expect(result.toString()).to.equal("[]");
   });
 
   it("ignores comments", () => {
     const input = "#Goodbye";
-    const result = evaluate(input);
+    const result = hc.evaluate(input);
     expect(result.toString()).to.equal(`[]`);
   });
 
   it("ignores spaces", () => {
     const input = "  ";
-    const result = evaluate(input);
+    const result = hc.evaluate(input);
     expect(result.toString()).to.equal(`[]`);
   });
 
   it("quines string literal", () => {
     const input = "“Hello, HC!”";
-    const result = evaluate(input);
+    const result = hc.evaluate(input);
     expect(result.toString()).to.equal(`[${input}]`);
   });
 
   it("quines string before spaces", () => {
     const input = "“Hello, HC!”";
     const suffix = `${input}  `;
-    const result = evaluate(suffix);
+    const result = hc.evaluate(suffix);
     expect(result.toString()).to.equal(`[${input}]`);
   });
 
   it("quines string after spaces", () => {
     const input = "“Hello, HC!”";
     const prefix = `  ${input}`;
-    const result = evaluate(prefix);
+    const result = hc.evaluate(prefix);
     expect(result.toString()).to.equal(`[${input}]`);
   });
 
   it("joins multiple strings", () => {
     const input = "“Hello”“, HC!”";
-    const result = evaluate(input);
+    const result = hc.evaluate(input);
     expect(result.toString()).to.equal(`[“Hello, HC!”]`);
   });
 
   it("joins around inner space", () => {
     const input = "“Hello” “, HC!”";
-    const result = evaluate(input);
+    const result = hc.evaluate(input);
     expect(result.toString()).to.equal(`[“Hello, HC!”]`);
   });
 
   it("joins multi-line doc-strings into strings", () => {
     const input = "```\nDoc String\n```";
-    const result = evaluate(input);
+    const result = hc.evaluate(input);
     expect(result.toString()).to.equal(`[“\nDoc String\n”]`);
   });
 
@@ -80,28 +79,29 @@ describe("evaluate", () => {
 
     it("evaluates in context", () => {
       const context: frame.Context = {key: frame_value};
-      const result = evaluate(key, context) as frame.FrameArray;
+      const hc2 = new HC(context);
+      const result = hc2.evaluate(key) as frame.FrameArray;
       expect(result.size()).to.equal(1);
       const output = result.at(0);
       expect(output.toString()).to.equal(frame_value.toString());
     });
 
     it("evaluates names to symbols", () => {
-      const result = evaluate(`.${key}`) as frame.FrameArray;
+      const result = hc.evaluate(`.${key}`) as frame.FrameArray;
       expect(result.size()).to.equal(1);
       const output = result.at(0);
       expect(output).to.be.instanceof(frame.FrameSymbol);
     });
 
     it("set symbols in result", () => {
-      const result = evaluate(setting) as frame.FrameArray;
+      const result = hc.evaluate(setting) as frame.FrameArray;
       const extracted = result.get(key);
       expect(extracted.toString()).to.equal(frame_value.toString());
     });
 
     it("evaluates created symbols", () => {
       const input = `${setting}\n${key}`;
-      const result = evaluate(input) as frame.FrameArray;
+      const result = hc.evaluate(input) as frame.FrameArray;
       // console.error(`result:`);
       // console.error(result);
 
