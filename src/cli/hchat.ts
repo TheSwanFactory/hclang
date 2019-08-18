e5f745de9c024f2f15cb41ac76794df9187f6534
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-18 08:34:20 -0700

    Create hchat.ts

---
+++ src/cli/hchat.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import * as prompt_sync from "prompt-sync";
+import * as prompt_history from "prompt-sync-history";
+import { HC } from "../execute/hc";
+import { Context, Frame, NilContext } from "../frames";
+
+const prompt = prompt_sync({
+  history: prompt_history(),
+});
+
+export class HChat {
+  public static readonly IN = "; ";
+  public static readonly OUT = "# ";
+
+  public static iterate(hc: HC): void {
+    const hchat = new HChat(hc);
+    hchat.call();
+  }
+
+  constructor(protected hc: HC) {
+  }
+
+  public call(): boolean {
+    let status = false;
+    while (status) {
+      const input = prompt(HChat.IN);
+      const output = this.hc.evaluate(input);
+      console.log(HChat.OUT + output);
+      status = true;
+    }
+    return true;
+  }
+}
