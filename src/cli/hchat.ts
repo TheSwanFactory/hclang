8808cdc346be07605188084f27ed28fd64d95cff
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-19 22:44:12 -0700

    DEBUG envar

--- src/cli/hchat.ts
+++ src/cli/hchat.ts
@@ -31,6 +31,9 @@ export class HChat {
         break;
       }
       const output = this.hc.evaluate(input);
+      if (this.hc.get("DEBUG")) {
+        console.log(output);
+      }
       console.log(HChat.OUT + output);
     }
     return status;
