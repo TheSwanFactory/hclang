b56aa32356cce78e5f4f6d36967862fbcec0441e
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-22 05:06:47 -0700

    ts:version

--- lib/frames/frame-number.d.ts
+++ lib/frames/frame-number.d.ts
@@ -4,7 +4,7 @@ export declare class FrameNumber extends FrameAtom {
     static readonly NUMBER_BEGIN = "0-9";
     static readonly NUMBER_END = "^0-9";
     protected data: number;
-    string_start(): string;
     constructor(source: string, meta?: Context);
+    string_start(): string;
     protected toData(): number;
 }
