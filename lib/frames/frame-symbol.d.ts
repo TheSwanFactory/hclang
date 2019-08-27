36a3362c8aada1f0678c779a5375c9a375816d09
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-08-26 18:09:12 -0700

    SYMBOL_BEGIN = /[-a-zA-Z]/;
    Stop symbol from eating numbers

--- lib/frames/frame-symbol.d.ts
+++ lib/frames/frame-symbol.d.ts
@@ -3,6 +3,7 @@ import { FrameAtom } from "./frame-atom";
 import { Context } from "./meta-frame";
 export declare class FrameSymbol extends FrameAtom {
     protected data: string;
+    static readonly SYMBOL_BEGIN: RegExp;
     static readonly SYMBOL_CHAR: RegExp;
     static for(symbol: string): FrameSymbol;
     static end(): FrameSymbol;
