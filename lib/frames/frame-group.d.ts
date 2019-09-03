f907514b12ee7a2e9bd86c9f499e2dcf9dd97577
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-09-03 07:10:34 -0700

    handle FrameGroup size 0

--- lib/frames/frame-group.d.ts
+++ lib/frames/frame-group.d.ts
@@ -1,7 +1,8 @@
 import { Frame } from "./frame";
-import { FrameList } from "./frame-list";
+import { FrameExpr } from "./frame-expr";
 import { Context } from "./meta-frame";
-export declare class FrameGroup extends FrameList {
+export declare class FrameGroup extends FrameExpr {
     constructor(data: Array<Frame>, meta?: Context);
+    eval_one(contexts?: Frame[]): Frame;
     in(contexts?: Frame[]): Frame;
 }
--- src/frames/frame-group.ts
+++ src/frames/frame-group.ts
@@ -1,23 +1,35 @@
 import { Frame } from "./frame";
-import { FrameList } from "./frame-list";
+import { FrameExpr } from "./frame-expr";
 import { Context, NilContext } from "./meta-frame";
 
-export class FrameGroup extends FrameList {
+export class FrameGroup extends FrameExpr {
   constructor(data: Array<Frame>, meta = NilContext) {
     super(data, meta);
   }
 
-  public in(contexts = [Frame.nil]): Frame {
-    if (this.size() > 1) {
-      return this.array_eval(contexts);
-    }
+  public eval_one(contexts = [Frame.nil]): Frame {
     const expr = this.data[0];
     contexts.push(this);
     const result = expr.in(contexts);
+    if (expr.is.statement) {
+      result.is.statement = true;
+    }
     const symbols = this.meta_pairs();
     symbols.map(([key, value]) => {
       result.set(key, value);
     });
     return result;
+  }
+
+  public in(contexts = [Frame.nil]): Frame {
+    switch (this.size()) {
+      case 0: {
+        return Frame.nil;
+      }
+      case 1: {
+        return this.eval_one(contexts);
+      }
+    }
+    return this.array_eval(contexts);
   }
 }
