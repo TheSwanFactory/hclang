672c5acb2c5b093aa606eb74d78c4190478c92a2
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-09-10 07:38:10 -0700

    stub HCTest.call

--- lib/execute/hc-test.d.ts
+++ lib/execute/hc-test.d.ts
@@ -1,5 +1,10 @@
 import { Frame } from "../frames";
+export declare type Counts = {
+    [key: string]: number;
+};
 export declare class HCTest extends Frame {
     protected out: Frame;
+    n: Counts;
     constructor(out: Frame);
+    call(actual: Frame, parameter?: Frame): Frame;
 }
--- src/execute/hc-test.ts
+++ src/execute/hc-test.ts
@@ -1,9 +1,28 @@
 // import * as fs from "fs";
 import { Context, Frame, FrameGroup, FrameString, NilContext } from "../frames";
+import { HCEval } from "./hc-eval";
+
+export type Counts = { [key: string]: number; };
 
 export class HCTest extends Frame {
 
+  public n: Counts;
+
   constructor(protected out: Frame) {
     super(NilContext);
-  } 
+    this.n = {total: 0, pass: 0, fail: 0};
+  }
+
+  public call(actual: Frame, parameter = Frame.nil): Frame {
+    const source = this.get(HCEval.SOURCE);
+    const expected = this.get(HCEval.EXPECT);
+
+    if (source.is.missing || expected.is.missing) {
+      return actual;
+    }
+
+    this.set(HCEval.SOURCE, Frame.missing);
+    this.set(HCEval.EXPECT, Frame.missing);
+    return this.out.call(actual, parameter);
+  }
 }
