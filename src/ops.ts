b863dc702defee38c1ace70583baca641e69a7f0
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-09-17 05:28:03 -0700

    ReduceEnumerable

--- src/ops.ts
+++ src/ops.ts
@@ -1,9 +1,9 @@
-import { Frame, FrameArg, FrameExpr } from "./frames";
 export { ICurryFunction } from "./ops/frame-curry";
 import { FrameOps } from "./ops/frame-ops";
-import { MapProperties, MapEnumerable } from "./ops/iterators";
+import { MapEnumerable, MapProperties, ReduceEnumerable } from "./ops/iterators";
 
 export const Ops = new FrameOps({
   "&":  MapEnumerable,
   "&&": MapProperties,
+  "|":  ReduceEnumerable,
 });
--- src/ops/iterators.ts
+++ src/ops/iterators.ts
@@ -1,13 +1,5 @@
 import { Frame, FrameArray, FrameNumber, FrameString } from "../frames";
 
-export const MapProperties = (source: Frame, block: Frame) => {
-  const array = source.meta_pairs().map( ([key, value]) => {
-    const fkey = new FrameString(key);
-    return block.call(value, fkey);
-  });
-  return new FrameArray(array);
-};
-
 export const MapEnumerable = (source: Frame, block: Frame) => {
   let i = 0;
   const array = source.asArray().map( (value) => {
@@ -16,4 +8,23 @@ export const MapEnumerable = (source: Frame, block: Frame) => {
     return block.call(value, param);
   });
   return new FrameArray(array);
+};
+
+export const MapProperties = (source: Frame, block: Frame) => {
+  const array = source.meta_pairs().map( ([key, value]) => {
+    const fkey = new FrameString(key);
+    return block.call(value, fkey);
+  });
+  return new FrameArray(array);
+};
+
+export const ReduceEnumerable = (source: Frame, block: Frame) => {
+  let i = 0;
+  const FrameReducer = (sum: Frame, value: Frame) => {
+    const param = FrameNumber.for(i.toString());
+    i += 1;
+    return sum.call(value, param);
+  };
+  const result = source.asArray().reduce(FrameReducer, block);
+  return result;
 };
