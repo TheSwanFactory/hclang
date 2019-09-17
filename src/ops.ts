25c6ba0ae13eb20752117b0edc226a76e72b02ea
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2019-09-16 20:14:06 -0700

    MapEnumerable

--- src/ops.ts
+++ src/ops.ts
@@ -1,8 +1,9 @@
 import { Frame, FrameArg, FrameExpr } from "./frames";
 export { ICurryFunction } from "./ops/frame-curry";
 import { FrameOps } from "./ops/frame-ops";
-import { MapProperties } from "./ops/iterators";
+import { MapProperties, MapEnumerable } from "./ops/iterators";
 
 export const Ops = new FrameOps({
+  "&":  MapEnumerable,
   "&&": MapProperties,
 });
--- src/ops/iterators.ts
+++ src/ops/iterators.ts
@@ -1,9 +1,19 @@
-import { Frame, FrameArray, FrameString } from "../frames";
+import { Frame, FrameArray, FrameNumber, FrameString } from "../frames";
 
 export const MapProperties = (source: Frame, block: Frame) => {
   const array = source.meta_pairs().map( ([key, value]) => {
     const fkey = new FrameString(key);
     return block.call(value, fkey);
+  });
+  return new FrameArray(array);
+};
+
+export const MapEnumerable = (source: Frame, block: Frame) => {
+  let i = 0;
+  const array = source.asArray().map( (value) => {
+    const param = FrameNumber.for(i.toString());
+    i += 1;
+    return block.call(value, param);
   });
   return new FrameArray(array);
 };
