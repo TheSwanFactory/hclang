5ac32eca0b121a0efe0dc86867400824aadbe7af
Ernest Prabhakar <ernest@quiltdata.io>
2023-10-03 21:15:18 -0700

    function runfile

--- src/cli/runfile.ts
+++ src/cli/runfile.ts
@@ -1,11 +1,16 @@
-RUNDOC="#!/usr/bin/env hc \n```\n"
+#!/usr/bin/env node
+import fs from 'fs'
+import readline from 'readline'
+import { HCEval } from '../execute/hc-eval.js'
 
-// prefix adoc/md input file with RUNDOC header
+const RUNDOC = '#!/usr/bin/env hc \n```\n'
 
- runfile(file) {
-    const rl = readline.createInterface(fs.createReadStream(file), undefined)
-    rl.on('line', (line) => {
-        hc_eval.call(line)
-    })
-    evaluated = true      
+function runfile (hc_eval: HCEval, file: any) {
+  const rl = readline.createInterface(fs.createReadStream(file), undefined)
+  rl.on('line', (line) => {
+    hc_eval.call(line)
+  })
+  return true
 }
+
+export { runfile }
