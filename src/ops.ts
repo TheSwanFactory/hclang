cc2b952c9bbce5c659990cdbe49795de6b09ea9d
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-15 12:29:38 -0800

    use .ts

--- src/ops.ts
+++ src/ops.ts
@@ -1,25 +1,41 @@
-import { IfElse, IfThen } from './ops/conditionals.js'
-import { FrameOps } from './ops/frame-ops.js'
-import { MapEnumerable, MapProperties, ReduceEnumerable } from './ops/iterators.js'
-import { Add, Divide, Equals, GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, Modulo, Multiply, Power, Subtract } from './ops/math.js'
+import { IfElse, IfThen } from "./ops/conditionals.ts";
+import { FrameOps } from "./ops/frame-ops.ts";
+import {
+  MapEnumerable,
+  MapProperties,
+  ReduceEnumerable,
+} from "./ops/iterators.ts";
+import {
+  Add,
+  Divide,
+  Equals,
+  GreaterThan,
+  GreaterThanOrEqual,
+  LessThan,
+  LessThanOrEqual,
+  Modulo,
+  Multiply,
+  Power,
+  Subtract,
+} from "./ops/math.ts";
 
-export { FrameCurry, ICurryFunction } from './ops/frame-curry.js'
+export { FrameCurry, ICurryFunction } from "./ops/frame-curry.ts";
 
 export const Ops = new FrameOps({
-  '&': MapEnumerable,
-  '&&': MapProperties,
-  ':': IfElse,
-  '?': IfThen,
-  '|': ReduceEnumerable,
-  '+': Add,
-  '-': Subtract,
-  '*': Multiply,
-  '/': Divide,
-  '%%': Modulo,
-  '**': Power,
-  '=': Equals,
-  '>>': GreaterThan,
-  '>=': GreaterThanOrEqual,
-  '<<': LessThan,
-  '<=': LessThanOrEqual
-})
+  "&": MapEnumerable,
+  "&&": MapProperties,
+  ":": IfElse,
+  "?": IfThen,
+  "|": ReduceEnumerable,
+  "+": Add,
+  "-": Subtract,
+  "*": Multiply,
+  "/": Divide,
+  "%%": Modulo,
+  "**": Power,
+  "=": Equals,
+  ">>": GreaterThan,
+  ">=": GreaterThanOrEqual,
+  "<<": LessThan,
+  "<=": LessThanOrEqual,
+});
