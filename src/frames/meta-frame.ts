6b07a68ad8411b71ac72a751376250a39847b82a
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-15 17:46:56 -0800

    contextString/contextEqual

--- src/frames/meta-frame.ts
+++ src/frames/meta-frame.ts
@@ -7,6 +7,31 @@ export interface ISourced extends Frame {
 export type Context = { [key: string]: Frame };
 export const NilContext: Context = {};
 
+export function contextEqual(
+  left: Context,
+  right: Context,
+): boolean {
+  const keys = new Set([...Object.keys(left), ...Object.keys(right)]);
+  for (const key of keys) {
+    if (left[key] !== right[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function contextString(
+  context: Context,
+): string {
+  return Object.entries(context).map(([key, value]) => {
+    if (key === Frame.kOUT) {
+      return `.${key} ${value.id};`;
+    } else {
+      return `.${key} ${value};`;
+    }
+  }).join(" ");
+}
+
 export interface IKeyValuePair extends ReadonlyArray<string | Frame> {
   0: string;
   1: Frame;
