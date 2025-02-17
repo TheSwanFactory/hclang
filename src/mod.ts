a81db3842973ecf6c4149d94d3de1c3628ab0fad
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-16 18:17:30 -0800

    export make_context

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,7 @@
 export { evaluate } from "./execute/evaluate.ts";
 export { execute } from "./execute/execute.ts";
+export { make_context } from "./execute/hc-eval.ts";
 export { getOptions } from "./cli/hc.ts";
 export { runfile } from "./cli/runfile.ts";
 // export { Flatten } from "./cli/flatten.ts";
-export { type IKeyValuePair, type Flags, type Context, Frame, MetaFrame } from "./frames.ts";
+export { type IKeyValuePair, type Flags, type Context, type StringMap, Frame, MetaFrame } from "./frames.ts";
