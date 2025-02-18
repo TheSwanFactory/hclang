98cc1608a79c2e44c8032f5a42eb042e06f18d6c
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-17 22:09:35 -0800

    test cli

--- cli/runfile.test.ts
+++ cli/runfile.test.ts
@@ -14,7 +14,7 @@ describe("runfile", () => {
   });
 
   it("calls hc_eval.call for each line in the file", async () => {
-    const file = "test/cli/testfile.txt";
+    const file = "./testfile.test.txt";
     await Deno.writeTextFile(file, "1 + 1\n2 + 2");
 
     let callCount = 0;
@@ -29,7 +29,7 @@ describe("runfile", () => {
   });
 
   it("handles empty files", async () => {
-    const emptyFile = "test/cli/emptyfile.txt";
+    const emptyFile = "./emptyfile.test.txt";
     await Deno.writeTextFile(emptyFile, "");
 
     let callCount = 0;
@@ -42,7 +42,7 @@ describe("runfile", () => {
   });
 
   it("handles files with a single line", async () => {
-    const singleLineFile = "test/cli/singlelinefile.txt";
+    const singleLineFile = "./singlelinefile.test.txt";
     await Deno.writeTextFile(singleLineFile, "1 + 1");
 
     let callCount = 0;
@@ -54,7 +54,7 @@ describe("runfile", () => {
     await Deno.remove(singleLineFile);
   });
   it("handles files with multiple lines", async () => {
-    const multiLineFile = "test/cli/multilinefile.txt";
+    const multiLineFile = "./multilinefile.test.txt";
     await Deno.writeTextFile(multiLineFile, "1 + 1\n2 + 2");
 
     let callCount = 0;
@@ -66,7 +66,7 @@ describe("runfile", () => {
     await Deno.remove(multiLineFile);
   });
   it("handles files with a trailing newline", async () => {
-    const trailingNewlineFile = "test/cli/trailingnewlinefile.txt";
+    const trailingNewlineFile = "./trailingnewlinefile.test.txt";
     await Deno.writeTextFile(trailingNewlineFile, "1 + 1\n");
 
     let callCount = 0;
