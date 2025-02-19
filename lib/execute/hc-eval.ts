7edfdfd5a816e354f2ac578f1a07503d58fc3ad0
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-19 06:31:51 -0800

    extract Prompt

--- lib/execute/hc-eval.ts
+++ lib/execute/hc-eval.ts
@@ -80,18 +80,6 @@ export class HCEval {
     return lexer;
   }
 
-  /**
-   * Generates a prompt string for the given level.
-   *
-   * @param {number} level - The level of indentation.
-   * @returns {string} The generated prompt string.
-   */
-  public static make_prompt(level: number): string {
-    const indent = 2 * (level - 1);
-    const middle = " ".repeat(indent);
-    return HCEval.EXPECT + middle + HCEval.EXPECT;
-  }
-
   protected pipe: LexPipe;
   protected lex: Frame;
 
@@ -115,41 +103,8 @@ export class HCEval {
     return result;
   }
 
-  /**
-   * Runs a Read-Eval-Print Loop (REPL) to continuously read input, evaluate it, and print the result.
-   *
-   * @returns {Promise<boolean>} A promise that resolves to `true` if the REPL was successful, `false` otherwise.
-   */
-  public async repl(): Promise<boolean> {
-    console.log(chalk.green(".hc " + VERSION + ";"));
-    let status = true;
-    for await (const input of this.getInputStream()) {
-      if (!input) {
-        status = false;
-        break;
-      }
-      this.call(input);
-    }
-    return status;
-  }
-
-  protected async *getInputStream(): AsyncGenerator<string, void, unknown> {
-    const decoder = new TextDecoderStream();
-    const stdinStream = Deno.stdin.readable.pipeThrough(decoder);
-    const reader = stdinStream.getReader();
-
-    while (true) {
-      let prefix = HCEval.SOURCE;
-      if (this.pipe.level > 0) {
-        prefix = HCEval.make_prompt(this.pipe.level);
-      }
-
-      await Deno.stdout.write(new TextEncoder().encode(chalk.grey(prefix)));
-
-      const { value, done } = await reader.read();
-      if (done) break;
-      yield value.trim();
-    }
+  public level(): number {
+    return this.pipe.level;
   }
 
   protected checkInput(input: string): void {
@@ -166,6 +121,59 @@ export class HCEval {
         this.out.set(HCEval.EXPECT, value);
         break;
       }
+    }
+  }
+}
+
+export class Prompt {
+  constructor(private hc_eval: HCEval) {}
+
+  /**
+   * Generates a prompt string for the given level.
+   *
+   * @param {number} level - The level of indentation.
+   * @returns {string} The generated prompt string.
+   */
+  public static make_prompt(level: number): string {
+    const indent = 2 * (level - 1);
+    const middle = " ".repeat(indent);
+    return HCEval.EXPECT + middle + HCEval.EXPECT;
+  }
+
+  /**
+   * Runs a Read-Eval-Print Loop (REPL) to continuously read input, evaluate it, and print the result.
+   *
+   * @returns {Promise<boolean>} A promise that resolves to `true` if the REPL was successful, `false` otherwise.
+   */
+  public async repl(): Promise<boolean> {
+    console.log(chalk.green(".hc " + VERSION + ";"));
+    let status = true;
+    for await (const input of this.getInputStream()) {
+      if (!input) {
+        status = false;
+        break;
+      }
+      this.hc_eval.call(input);
+    }
+    return status;
+  }
+
+  protected async *getInputStream(): AsyncGenerator<string, void, unknown> {
+    const decoder = new TextDecoderStream();
+    const stdinStream = Deno.stdin.readable.pipeThrough(decoder);
+    const reader = stdinStream.getReader();
+
+    while (true) {
+      let prefix = HCEval.SOURCE;
+      if (this.hc_eval.level() > 0) {
+        prefix = Prompt.make_prompt(this.hc_eval.level());
+      }
+
+      await Deno.stdout.write(new TextEncoder().encode(chalk.grey(prefix)));
+
+      const { value, done } = await reader.read();
+      if (done) break;
+      yield value.trim();
     }
   }
 }
