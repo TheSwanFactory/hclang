b929b1d3e5c6172e6ba49128166b9b9a32ba7447
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-22 07:36:06 -0800

    remove dupe header

--- web/routes/local.tsx
+++ web/routes/local.tsx
@@ -3,17 +3,6 @@ import Main from "../islands/Main.tsx";
 export default function Local() {
   return (
     <div>
-      <header>
-        <h1>Welcome to Homoiconic C!</h1>
-        <p>Homoiconic C ("HC") is a universal data format for computation.</p>
-        <a
-          className="cheat-sheet"
-          href="https://ihack.us/2024/09/19/tsm-5-homoiconic-c-hc-syntax-cheat-sheet/"
-          target="_blank"
-        >
-          HC Cheat Sheet
-        </a>
-      </header>
       <main className="interpreter-wrapper">
         <h3>HCLang Interpreter</h3>
         <Main />
