2f57615f6adf2ed480749ff20b0585b3423b269f
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2025-02-23 07:08:04 -0800

    Fix jsr (#259)
    * remove web export
    
    * Update deno.lock
    
    * make web its own module
    
    * deno web publish
    
    ---------
    
    Co-authored-by: Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>

--- web/islands/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Main } from "./Main.tsx";
-export { default as Reset } from "./Reset.tsx";
-export { default as Executor } from "./Executor.tsx";
-export { default as Historian } from "./Historian.tsx";
---
+++ web/mod.ts
@@ -0,0 +1,4 @@
+export { default as Main } from "./islands/Main.tsx";
+export { default as Reset } from "./islands/Reset.tsx";
+export { default as Executor } from "./islands/Executor.tsx";
+export { default as Historian } from "./islands/Historian.tsx";
