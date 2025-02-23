e78a4f1b412bc71207bca2e2f4994285de6a8f53
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-22 20:20:23 -0800

    add HTML documentation

--- web/routes/_app.tsx
+++ web/routes/_app.tsx
@@ -16,13 +16,22 @@ export default function App({ Component }: PageProps) {
         <header>
           <h1>Welcome to Homoiconic C!</h1>
           <p>Homoiconic C ("HC") is a universal data format for computation.</p>
-          <a
-            className="cheat-sheet"
-            href="https://ihack.us/2024/09/19/tsm-5-homoiconic-c-hc-syntax-cheat-sheet/"
-            target="_blank"
-          >
-            HC Cheat Sheet
-          </a>
+          <div className="docs-links">
+            <h4>Documentation</h4>
+            <ul>
+              <li>
+                <a href="./BitScheme.html">Homoiconic C Tutorial (BitScheme)</a>
+              </li>
+              <li>
+                <a href="./hc-paper.html">Homoiconic C White Paper</a>
+              </li>
+              <li>
+                <a href="https://ihack.us/2024/09/19/tsm-5-homoiconic-c-hc-syntax-cheat-sheet/">
+                  HC Cheat Sheet
+                </a>
+              </li>
+            </ul>
+          </div>
         </header>
         <main>
           <Component />
--- web/routes/local.tsx
+++ web/routes/local.tsx
@@ -6,12 +6,6 @@ export default function Local() {
       <main className="interpreter-wrapper">
         <h3>HCLang Interpreter</h3>
         <Main />
-        <div className="docs-links">
-          <h4>Documentation</h4>
-          <ul>
-            <li><a href="/static/hc-paper.html">HC Paper</a></li>
-          </ul>
-        </div>
       </main>
     </div>
   );
